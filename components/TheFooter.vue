09af3f3c4217faae7710dec378079c2f4faf8bcb
이병준 <bjlee@insilicogen.com>
2021-04-09 14:30:14 +0900

    1) 레이아웃 구성 및 부트스트랩 적용

---
+++ components/TheFooter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card footer">
+        <div class="h2 mb-0">
+    <b-icon icon="exclamation-circle-fill" variant="success"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="warning"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="danger"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="info"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="primary"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="secondary"></b-icon>
+    <b-icon icon="exclamation-circle-fill" variant="dark"></b-icon>
+  </div>
+    </div>
+</template>
+
+<style scoped>
+.footer {
+    position: fixed;
+    height: 5%;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #F4EBEA;
+    color: white;
+    text-align: right;
+}
+</style>
---
+++ components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="card header">
+        <h1>Header!</h1>
+    </div>
+</template>
+
+<style scoped>
+.header {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    background-color: #E6E2B3;
+    color: white;
+    text-align: center;
+}
+</style>
